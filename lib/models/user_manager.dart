59c1315648d4d2bb1184f5b99ddce0aa60cc5b64
hugo <hugorc10@hotmail.com>
2021-07-19 00:23:46 -0300

    Adicionado ChangeNotifier, Consumidor e Builder.

--- lib/models/user_manager.dart
+++ lib/models/user_manager.dart
@@ -1,22 +1,33 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:loja_virtual/helpers/firebase_errors.dart';
 import 'package:loja_virtual/models/users.dart';
 
-class UserManager {
+class UserManager extends ChangeNotifier {
 
   final FirebaseAuth auth = FirebaseAuth.instance;
 
+  bool loading = false;
+
   Future<void> signIn({Users? users, required Function onFail, Function? onSuccess}) async {
+    setLoading(true);
     try {
       final UserCredential result = await auth.signInWithEmailAndPassword(
           email: users!.email, password: users.password);
 
-      print(result.user!.uid);
+      // await Future.delayed(Duration(seconds: 4));
 
       onSuccess!();
     } on FirebaseAuthException catch(e) {
       print(getErrorString(e.code));
       onFail(getErrorString(e.code));
     }
+
+    setLoading(false);
+  }
+
+  void setLoading(bool value) {
+    loading = value;
+    notifyListeners();
   }
 }
--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -32,93 +32,115 @@ class LoginScreen extends StatelessWidget {
           margin: const EdgeInsets.symmetric(horizontal: 16),
           child: Form(
             key: formKey,
-            child: ListView(
-              padding: const EdgeInsets.all(16),
-              shrinkWrap: true,
-              children: <Widget>[
-                TextFormField(
-                  controller: emailController,
-                  decoration: const InputDecoration(hintText: 'E-mail'),
-                  keyboardType: TextInputType.emailAddress,
-                  autocorrect: false,
-                  validator: (email) {
-                    if(!emailValid(email!)) {
-                      return 'E-mail invalido';
-                    } else {
-                      return null;
-
-                    }
-                  },
-
-                ),
-                const SizedBox(height: 16,),
-                TextFormField(
-                  controller: passwordController,
-                  decoration: const InputDecoration(hintText: 'Senha'),
-                  keyboardType: TextInputType.emailAddress,
-                  autocorrect: false,
-                  obscureText: true,
-                  validator: (password) {
-                    if(password!.isEmpty || password.length < 5) {
-                      return 'Senha invalida';
-                    }
-                    return null;
-                  },
-                ),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: TextButton(
-                    style: flatButtonStyle,
-                    onPressed: () {
-
-                    },
-                    child: const Text(
-                      'Esqueci minha senha'
+            child: Consumer<UserManager>(
+              builder: (_, userManager, child) {
+                return ListView(
+                  padding: const EdgeInsets.all(16),
+                  shrinkWrap: true,
+                  children: <Widget>[
+                    TextFormField(
+                      controller: emailController,
+                      enabled: !userManager.loading,
+                      decoration: const InputDecoration(hintText: 'E-mail'),
+                      keyboardType: TextInputType.emailAddress,
+                      autocorrect: false,
+                      validator: (email) {
+                        if(!emailValid(email!)) {
+                          return 'E-mail invalido';
+                        } else {
+                          return null;
+                        }
+                      },
                     ),
-                  ),
-                ),
-                const SizedBox(height: 16,),
-                SizedBox(
-                  height: 44,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      if(formKey.currentState!.validate()) {
-                        context.read<UserManager>().signIn(
-                          users: Users(
-                            email: emailController.text,
-                            password: passwordController.text
-                          ), onFail: (e) {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(
-                                content: Text('Falha ao entrar: $e'),
-                                backgroundColor: Colors.red,
-                                duration: Duration(seconds: 3),
-                              )
+                    const SizedBox(height: 16,),
+                    TextFormField(
+                      controller: passwordController,
+                      enabled: !userManager.loading,
+                      decoration: const InputDecoration(hintText: 'Senha'),
+                      keyboardType: TextInputType.emailAddress,
+                      autocorrect: false,
+                      obscureText: true,
+                      validator: (password) {
+                        if(password!.isEmpty || password.length < 5) {
+                          return 'Senha invalida';
+                        }
+                        return null;
+                      },
+                    ),
+                    child,
+                    const SizedBox(height: 16,),
+                    SizedBox(
+                      height: 44,
+                      child: ElevatedButton(
+                        onPressed: userManager.loading ? null : () {
+                          if(formKey.currentState!.validate()) {
+                            userManager.signIn(
+                                users: Users(
+                                    email: emailController.text,
+                                    password: passwordController.text
+                                ), onFail: (e) {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                    content: Text('Falha ao entrar: $e'),
+                                    backgroundColor: Colors.red,
+                                    duration: Duration(seconds: 3),
+                                  )
+                              );
+                            },
+                            onSuccess: () {
+                              // TODO: FECHAR TELA DE LOGIN
+                            }
                             );
-                        },
-                          onSuccess: () {
-                            // TODO: FECHAR TELA DE LOGIN
                           }
-                        );
-                      }
-                    },
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all<Color>(Theme.of(context).primaryColor),
-                    ),
-                    child: const Text(
-                        'Entrar',
-                      style: TextStyle(
-                        fontSize: 18
+                        },
+                        style: ButtonStyle(
+                          // backgroundColor: MaterialStateProperty.all<Color>(Theme.of(context).primaryColor),
+                          backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                            (Set<MaterialState> states) {
+                              if(states.contains(MaterialState.pressed)) {
+                                return Theme.of(context)
+                                    .primaryColor;
+                              } else if(states.contains(MaterialState.disabled)) {
+                                return Theme.of(context)
+                                    .primaryColor
+                                    .withAlpha(100);
+                              }
+                              return Theme.of(context).primaryColor;
+                            },
+                          ),
+                        ),
+                        child: userManager.loading ?
+                        CircularProgressIndicator(
+                          valueColor: AlwaysStoppedAnimation(Colors.white),
+                        ) :
+                        const Text(
+                          'Entrar',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18
+                          ),
+                        ),
                       ),
-                    ),
+                    )
+                  ],
+                );
+              },
+              child: Align(
+                alignment: Alignment.centerRight,
+                child: TextButton(
+                  style: flatButtonStyle,
+                  onPressed: () {
+
+                  },
+                  child: const Text(
+                      'Esqueci minha senha'
                   ),
-                )
-              ],
+                ),
+              ),
             ),
           ),
         ),
       ),
     );
   }
-
 }
